70336d0ace9bde49aef024fe97fef4ec3e72ebd7
YashShrivastava10 <shrivastavayash10@gmail.com>
2024-12-13 10:04:53 +0530

    keyboard shortcut for search + Responsive

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import { SearchIcon } from "lucide-react";
 import {
@@ -12,27 +12,43 @@ import {
 
 const Search = () => {
   const [open, setOpen] = useState(false);
+
+  // Keyboard shortcut for search using ctrl k
+  useEffect(() => {
+    const handleQuickSearch = (e: KeyboardEvent) => {
+      if (e.ctrlKey && (e.key === "k" || e.key === "K")) {
+        e.preventDefault();
+        setOpen((prevOpen) => !prevOpen);
+      }
+    };
+
+    document.addEventListener("keydown", handleQuickSearch);
+
+    return () => document.removeEventListener("keydown", handleQuickSearch);
+  }, []);
+
   return (
     <>
       <Button
         variant="outline"
-        className="relative flex justify-between w-64 text-muted-foreground"
+        className="relative flex justify-between w-40 md:w-64 text-muted-foreground"
         onClick={() => setOpen(true)}
       >
         <div className="flex items-center gap-4">
           <SearchIcon />
-          Quick Search...
+          <label className="hidden md:block">Quick Search...</label>
+          <label className="block md:hidden">Search...</label>
         </div>
-        <label>Ctrl k</label>
+        <label className="hidden md:block">Ctrl k</label>
       </Button>
       <CommandDialog open={open} onOpenChange={setOpen}>
-        <CommandInput placeholder="Type a command or search..." />
+        <CommandInput placeholder="Search Cities..." />
         <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandEmpty>No city found.</CommandEmpty>
           <CommandGroup heading="Suggestions">
-            <CommandItem>Calendar</CommandItem>
-            <CommandItem>Search Emoji</CommandItem>
-            <CommandItem>Calculator</CommandItem>
+            <CommandItem>Hyderabad</CommandItem>
+            <CommandItem>Pune</CommandItem>
+            <CommandItem>Delhi</CommandItem>
           </CommandGroup>
         </CommandList>
       </CommandDialog>
